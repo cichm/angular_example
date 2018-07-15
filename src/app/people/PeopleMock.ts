1e4ea0c9aaaa4b794add41dc1d96ee2c19ffe9e1
Mieszko Cichon <mieszkocichon@gmail.com>
2018-07-15 10:27:11 +0200

    People data change signature.

--- src/app/people/PeopleMock.ts
+++ src/app/people/PeopleMock.ts
@@ -1,14 +1,212 @@
 import { People } from "@app/people/People";
 
 export const PeopleMock: People[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  {
+    gender: "female",
+    name: {
+      "title": "miss",
+      "first": "aureliza",
+      "last": "moura"
+    },
+    location: {
+      street: "3386 rua treze ",
+      city: "duque de caxias",
+      state: "ceará",
+      postcode: 30151,
+      coordinates: {
+        latitude: "-74.7432",
+        longitude: "31.1556"
+      },
+      timezone: {
+        offset: "-7:00",
+        description: "Mountain Time (US & Canada)"
+      }
+    },
+    email: "aureliza.moura@example.com",
+    login: {
+      uuid: "1a335d43-32f2-44a3-a0d3-f470c2f05510",
+      username: "brownzebra344",
+      password: "sponge",
+      salt: "k9hpQmxa",
+      md5: "29c6b5db728de5ac8366a8235ef45f4b",
+      sha1: "3af2ddbbd77aa8d2c39aa1d1d8ad3378fd5f3505",
+      sha256: "aa7504d3b0f71642409f69cba9ded81e85a12e217a38e3c3f064a7d156e4af34"
+    },
+    dob: {
+      date: "1970-03-26T19:57:03Z",
+      age: 48
+    },
+    registered: {
+      date: "2002-05-12T12:54:27Z",
+      age: 16
+    },
+    phone: "(19) 0331-0268",
+    cell: "(82) 2031-9748",
+    id: {
+      name: "",
+      value: null
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/76.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/76.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/76.jpg"
+    },
+    nat: "BR"
+  },
+  {
+    gender: "female",
+    name: {
+      title: "miss",
+      first: "caroline",
+      last: "chavez"
+    },
+    location: {
+      street: "8666 york road",
+      city: "carlisle",
+      state: "warwickshire",
+      postcode: "UD92 3XH",
+      coordinates: {
+        latitude: "-40.1666",
+        longitude: "64.3644"
+      },
+      timezone: {
+        offset: "-3:30",
+        description: "Newfoundland"
+      }
+    },
+    email: "caroline.chavez@example.com",
+    login: {
+      uuid: "de43062f-b7d9-4d32-84df-3e51110db997",
+      username: "crazykoala350",
+      password: "idontkno",
+      salt: "fffb2SKM",
+      md5: "f0260f92989d929588fd4b76e18eb53e",
+      sha1: "f3cd35a6858ec438d8e3aad7df5ffcc4c5a88026",
+      sha256: "483a3fb545d9effe1264a2d496ac7f31cda6b1073f19590c910c99641f0a3e1c"
+    },
+    dob: {
+      date: "1987-04-05T03:18:47Z",
+      age: 31
+    },
+    registered: {
+      date: "2004-07-29T15:07:38Z",
+      age: 13
+    },
+    phone: "029 0407 5186",
+    cell: "0756-329-107",
+    id: {
+      name: "NINO",
+      value: "OR 19 48 21 K"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/90.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/90.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/90.jpg"
+    },
+    nat: "GB"
+  },
+  {
+    gender: "male",
+    name: {
+      title: "mr",
+      first: "tim",
+      last: "ferguson"
+    },
+    location: {
+      street: "9748 elgin st",
+      city: "tamworth",
+      state: "south australia",
+      postcode: "4175",
+      coordinates: {
+        latitude: "-41.7966",
+        longitude: "-20.2820"
+      },
+      timezone: {
+        offset: "+2:00",
+        description: "Kaliningrad, South Africa"
+      }
+    },
+    email: "tim.ferguson@example.com",
+    login: {
+      uuid: "4c2e0d78-60d0-489e-9e3a-6044d32389f5",
+      username: "blackpeacock830",
+      password: "shawna",
+      salt: "bxosSl80",
+      md5: "10ee486448a11f2de4abd69bdad7c2b7",
+      sha1: "6d0ebccf71d1696612d46f7ed08ebea51a38ce99",
+      sha256: "46ec01689af8eccba16680d4570996918b24bf12492c0a777a4e94204d777ad4"
+    },
+    dob: {
+      date: "1995-08-28T16:39:56Z",
+      age: 22
+    },
+    registered: {
+      date: "2003-01-29T17:28:42Z",
+      age: 15
+    },
+    phone: "04-0239-9152",
+    cell: "0452-730-277",
+    id: {
+      name: "TFN",
+      value: "068473687"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/men/87.jpg",
+      medium: "https://randomuser.me/api/portraits/med/men/87.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/men/87.jpg"
+    },
+    nat: "AU"
+  },
+  {
+    gender: "female",
+    name: {
+      title: "miss",
+      first: "inmaculada",
+      last: "lorenzo"
+    },
+    location: {
+      street: "940 avenida de burgos",
+      city: "torrevieja",
+      state: "andalucía",
+      postcode: 82430,
+      coordinates: {
+        latitude: "41.7610",
+        longitude: "140.7396"
+      },
+      timezone: {
+        offset: "+3:30",
+        description: "Tehran"
+      }
+    },
+    email: "inmaculada.lorenzo@example.com",
+    login: {
+      uuid: "7cde5c90-8e5e-4b87-8776-81d3941c9b8f",
+      username: "crazymouse596",
+      password: "morgan1",
+      salt: "ajjoMl3M",
+      md5: "5fada256f37ec2dc8eb96f803703ba2e",
+      sha1: "d782907b17675ac297118229f7f146c1ce6d24f7",
+      sha256: "ed365227133074ca06d4a9a80ba2cbb3cd0f360c433dfa7b6998ffb8ea67f8b9"
+    },
+    dob: {
+      date: "1985-10-22T18:11:22Z",
+      age: 32
+    },
+    registered: {
+      date: "2009-02-10T11:30:36Z",
+      age: 9
+    },
+    phone: "985-184-143",
+    cell: "629-838-551",
+    id: {
+      name: "DNI",
+      value: "19463357-T"
+    },
+    picture: {
+      large: "https://randomuser.me/api/portraits/women/73.jpg",
+      medium: "https://randomuser.me/api/portraits/med/women/73.jpg",
+      thumbnail: "https://randomuser.me/api/portraits/thumb/women/73.jpg"
+    },
+    nat: "ES"
+  }
 ];
